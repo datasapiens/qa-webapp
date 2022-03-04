d8f836bb1f766fd22fa8f8f43d602d4806febccd
mahanadh <mahan2054@gmail.com>
2022-03-04 18:35:07 +0545

    Add comments in commands for better understanding

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -1,4 +1,9 @@
 Cypress.Commands.add('signUp', (username, password) => {
+     /**
+     * Signup
+     * @param username — A user name for the user that needs to sign up
+     * @param password — A password for the user that needs to sign up
+     */
     cy.visit('/signup');
     cy.get("input[type='username']").type(username)
     cy.get(".sc-pVTFL.fpjaXA").eq(1).type(password)
@@ -7,6 +12,11 @@ Cypress.Commands.add('signUp', (username, password) => {
 })
 
 Cypress.Commands.add('signIn', (username, password) => {
+     /**
+     * Login
+     * @param username — The user name for first user for login
+     * @param password — The password for first user for login
+     */
     cy.visit("/login")
     cy.get("input[type='username']").type(username)
     cy.get("input[type='password']").type(password)
@@ -14,6 +24,11 @@ Cypress.Commands.add('signIn', (username, password) => {
 })
 
 Cypress.Commands.add('addBudgets', (budgetForUserOne, budgetForUserTwo) => {
+    /**
+     * Add budget for a user
+     * @param budgetForUserOne — Name of the first budget of first user
+     * @param budgetForUserTwo — Name of the second budget of second user
+     */
     cy.get(".sc-pVTFL.fpjaXA").type(budgetForUserOne)
     cy.get(".sc-kDTinF.iOwzpW").should('not.be.disabled').click()
     cy.get(".sc-pVTFL.fpjaXA").type(budgetForUserTwo)
@@ -21,6 +36,14 @@ Cypress.Commands.add('addBudgets', (budgetForUserOne, budgetForUserTwo) => {
 })
 
 Cypress.Commands.add('addCategory', (userCategory, categoryName, categoryDOMValue, addedExpense, user ) => {
+    /**
+     * Add Category for a user
+     * @param userCategory — Name of the category that user adds
+     * @param categoryName — Name of the category that user selects for the dropdown
+     * @param categoryDOMValue — The DOM value of a category for assertion
+     * @param addedExpense — The expense that is either added or substracted for a category
+     * @param user — user flag to substract expense for one of the category
+     */
     cy.get(".sc-iCfMLu.jilgKd .sc-pVTFL.fpjaXA").type(userCategory)
     cy.get("select").select(categoryName).should("have.value", categoryDOMValue)
     if(user){
@@ -31,6 +54,15 @@ Cypress.Commands.add('addCategory', (userCategory, categoryName, categoryDOMValu
 })
 
 Cypress.Commands.add('verifyStats', (budget, user, addedIncome, flag) => {
+    /**
+     * Verify the expense added for both users with that displayed in stats page
+     * @param budget — Name of the budget to select from the dropdown
+     * @param user — Username of the user to select from the dropdown
+     * @param addedIncome — One of the income which is either an addition or 
+     * reduction so that it can be compared with that in the stats page
+     * @param flag — A flag to distinguish between the two users because each 
+     * user's stat data needs different split functionm
+     */
     cy.visit("/stats")
     cy.get(".sc-jrQzAO.hDXECM").eq(0).select(budget)
     cy.get(".sc-jrQzAO.hDXECM").eq(1).select(user)
@@ -38,13 +70,13 @@ Cypress.Commands.add('verifyStats', (budget, user, addedIncome, flag) => {
         var addedExpenseForUser = text
         if(flag){
             var expenseForUser = addedExpenseForUser.split(/\s+/);
-            var flag = expenseForUser[1] == addedIncome
+            var correctStat = expenseForUser[1] == addedIncome
         }
         else{
             var expenseForUser = addedExpenseForUser.split(' ')
-            var flag = expenseForUser[2] == addedIncome
+            var correctStat = expenseForUser[2] == addedIncome
         }
-        if(flag){
+        if(correctStat){
             cy.task('log', "The stats of"+user+"is correct")
         }
         else{
@@ -54,6 +86,9 @@ Cypress.Commands.add('verifyStats', (budget, user, addedIncome, flag) => {
 })
 
 Cypress.Commands.add('deleteBudget', () => {
+    /**
+     * Delete all the budget(both - userOne and userTwo)
+     */
     cy.visit("/lists")
     cy.get(".sc-kDTinF.kEvPAM").click({ multiple: true })
 })
